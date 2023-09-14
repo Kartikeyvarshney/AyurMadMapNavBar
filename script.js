db91b04a6611e28d54459bfe3e2b011962dafd8d
KARTIKEY VARSHNEY <kartikeyvarshney5252@gmail.com>
2023-09-14 23:40:13 +0530

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const menu = document.querySelector('.btn')
 const sidebar = document.querySelector('aside')
+const nav = document.querySelector('.desktop-nav')
 menu.onclick = () => {
     sidebar.classList.toggle('active')
 }
-// let count = 1;
-setInterval(() => {
-    if (screen.width >780) {
-
+window.onresize = ()=>{
+    if (window.innerWidth >780) {
         if (sidebar.classList.contains('active')) {
-           
             menu.click()
-            count = count+1;
 
         }
     }
-
-}, 500)
+}
